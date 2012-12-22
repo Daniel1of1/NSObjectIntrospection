2c29c99d3fa55ad206f9237e95b20e29f1911dfb
Confidence <confidence.designed@gmail.com>
2012-12-22 01:05:10 +0000

    more file organisation

---
+++ NSObjectIntrospectionDemoTests/NSObjectIntrospectionDemoTests.m
@@ -0,0 +1,32 @@
+//
+//  NSObjectIntrospectionDemoTests.m
+//  NSObjectIntrospectionDemoTests
+//
+//  Created by confidence on 21/12/2012.
+//  Copyright (c) 2012 confidence. All rights reserved.
+//
+
+#import "NSObjectIntrospectionDemoTests.h"
+
+@implementation NSObjectIntrospectionDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NSObjectIntrospectionDemoTests");
+}
+
+@end
