0551d60b110bc66e6e7f975de4f9cd626c17bcc3
Confidence <confidence.designed@gmail.com>
2012-12-22 01:29:38 +0000

    add test files

---
+++ NSObjectIntrospectionDemoTests/NSObject+IntrospectTests.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+IntrospectTests.h
+//  NSObjectIntrospectionDemo
+//
+//  Created by confidence on 22/12/2012.
+//  Copyright (c) 2012 confidence. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NSObject_IntrospectTests : SenTestCase
+
+@end
---
+++ NSObjectIntrospectionDemoTests/NSObject+IntrospectTests.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+IntrospectTests.m
+//  NSObjectIntrospectionDemo
+//
+//  Created by confidence on 22/12/2012.
+//  Copyright (c) 2012 confidence. All rights reserved.
+//
+
+#import "NSObject+IntrospectTests.h"
+
+@implementation NSObject_IntrospectTests
+
+@end
