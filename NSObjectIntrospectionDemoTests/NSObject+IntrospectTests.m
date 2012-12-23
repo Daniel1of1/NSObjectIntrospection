51ee0c6fafa15fa68c7e9949bf127364e3792351
Confidence <confidence.designed@gmail.com>
2012-12-23 00:08:10 +0000

    add iVarDict test

--- NSObjectIntrospectionDemoTests/NSObject+IntrospectTests.m
+++ NSObjectIntrospectionDemoTests/NSObject+IntrospectTests.m
@@ -56,4 +56,33 @@
 
 }
 
+
+-(void)testIVarsDictSetsCorrectDictionary{
+    demoObj.aString=@"some string value";
+    demoObj.aFloat=8;
+    demoObj.aNumber=[NSNumber numberWithDouble:M_PI];
+    demoObj.aLeafObject=[[DemoLeafObject alloc] init];
+    DemoLeafObject *leaf1=[[DemoLeafObject alloc] init];
+    DemoLeafObject *leaf2=[[DemoLeafObject alloc] init];
+    DemoLeafObject *leaf3=[[DemoLeafObject alloc] init];
+    demoObj.arrayOfLeaves=[NSArray arrayWithObjects:leaf1, leaf2, leaf3,@"another string value", nil];
+    demoObj.dictOfLeaves=[NSDictionary dictionaryWithObjects:demoObj.arrayOfLeaves forKeys:[NSArray arrayWithObjects:@"key1",@"key2",@"key3",@"key4", nil]];
+    
+    NSDictionary *propsDict=[demoObj iVarsDict];
+    
+    NSDictionary *correctDictionary=@{
+    @"anIvarString" : [demoObj valueForKey:@"anIvarString"],
+    @"aString" : [demoObj valueForKey:@"aString"],
+    @"aNumber" : [demoObj valueForKey:@"aNumber"],
+    @"aFloat" : [demoObj valueForKey:@"aFloat"],
+    @"aLeafObject" : [demoObj valueForKey:@"aLeafObject"],
+    @"arrayOfLeaves" : [demoObj valueForKey:@"arrayOfLeaves"],
+    @"dictOfLeaves" : [demoObj valueForKey:@"dictOfLeaves"]
+    };
+    
+    STAssertTrue([propsDict isEqualToDictionary:correctDictionary] , @"propertiesDict should return correct dictionary it looks like this %@\n it should be this %@",propsDict.description,correctDictionary.description);
+
+}
+
+
 @end
